a712588fe1069e2b2a4c4eaa22e70a47f085c16a
Nicolás Fernández Rodríguez <64916539+NIKOAFK@users.noreply.github.com>
2022-02-25 12:58:27 -0500

    Update task.py

--- models/task.py
+++ models/task.py
@@ -3,9 +3,9 @@ from models import machine as machine
 
 class task:
 
-    def __init__(self, timeTask, machine:machine):
+    def __init__(self, timeTask, machine):
         self.timeTask = timeTask
-        self.machine = machine.machine
+        self.machine = machine
         
 
     def getTimeTask(self):
