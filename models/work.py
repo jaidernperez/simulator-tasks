2857c057ac29a1c892e7b123240214fa3e0bd7b1
Nicolás Fernández Rodríguez <64916539+NIKOAFK@users.noreply.github.com>
2022-03-01 22:03:56 -0500

    Update work.py

--- models/work.py
+++ models/work.py
@@ -1,31 +1,51 @@
-from models.task import Task
+from models.machine import Machine
 
 
-class Work:
-    name: str
-    tasks: list[Task]
+class Task:
+    machine: Machine
+    time_task: int
     state: str
+    start_time: int
+    end_time: int
 
-    def __init__(self, name_p, tasks_p):
-        self.name = name_p
-        self.tasks = tasks_p
+    def __init__(self, machine_p, time_task_p):
+        self.machine = machine_p
+        self.time_task = time_task_p
+        self.start_time = 0
+        self.end_time = 0
         self.set_initial_state()
 
     def set_initial_state(self):
-        # States: WORK_AVAILABLE, WORK_DOING, WORK_FINISHED
-        super()
+        # states: TASK_AVAILABLE, TASK_DOING, TASK_FINISHED
+        self.state = "TASK_AVAILABLE"
 
     def is_available(self):
-        super()
+        if self.time_task == 0:
+            self.state = "TASK_FINISHED"
+        return self.state == "TASK_AVAILABLE"
+
+    def is_doing(self):
+        return self.state == "TASK_DOING"
 
     def is_finished(self):
-        super()
+        return self.state == "TASK_FINISHED"
 
-    def _is_tasks_already_finished(self):
-        super()
+    def do_task(self, time_p):
+        if self.machine.is_available():
+            self.state = "TASK_DOING"
+            self.machine.set_state("MACHINE_BUSY")
+            self.start_time = time_p
+            self.end_time = self.time_task + time_p
+            return True
+        return False
 
-    def do_task_available(self, time):
-        super()
+    def change_state_if_has_finished(self, time_p):
+        if time_p >= self.end_time:
+            self.state = "TASK_FINISHED"
+            self.machine.set_state("MACHINE_AVAILABLE")
+            return True
+        return False
 
     def __str__(self):
-        return f"(Name: {self.name}, tasks: {[task.__str__() for task in self.tasks]}, state: {self.state})"
+        return f"(Machine: {self.machine.__str__()}, time_task: {self.time_task}, state: {self.state}," \
+               f" start time: {self.start_time}, end_time: {self.end_time})"
